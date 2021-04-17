743f203f0fe0dc8c98e0246628be29e1342e797d
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-16 20:01:59 -0600

    Finished!

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const emailInput = document.querySelector('#email');
 
 const emailList = [];
 
+const formContainer = document.querySelector('.form-container');
+
+const cart = document.createElement('div');
+const main = document.querySelector('main');
+let cartItems = 0;
+
 function toggler(){
-    console.log(nameInput);
     form.classList.toggle('hide');
     formBtn.innerText === 'X' ? formBtn.innerText = '+' : formBtn.innerText = 'X';
 }
@@ -15,15 +20,57 @@ function toggler(){
 function validateForm(){
     if (!nameInput.value && !emailInput.value) {
         nameInput.style.border = 'red 2px solid'
-        // window.alert('you must enter a name and an email address to subscribe!')
+        emailInput.style.border = 'red 2px solid'
+        window.alert('you must enter a name and an email address to subscribe!')
     }
     else if(!nameInput.value && emailInput.value){
+        nameInput.style.border = 'red 2px solid'
+        window.alert('you must enter a name to subscribe!')
 
     }
     else if(nameInput.value && !emailInput.value){
+        emailInput.style.border = 'red 2px solid'
+        window.alert('you must enter an email address to subscribe!')
 
     }
     else{
-
+        emailList.push({name: nameInput.value, email: emailInput.value})
+        displayThankYou();
     }
+}
+
+const displayThankYou = function(){
+    formContainer.innerText = 'Thank you for subscribing!';
+    setTimeout(()=>formContainer.remove(formContainer),3000);
+}
+
+
+form.addEventListener('submit',event=>{
+    event.preventDefault(validateForm);
+})
+
+emailInput.addEventListener('change', ()=>{
+    emailInput.style.border = 'none'
+})
+nameInput.addEventListener('change', ()=>{
+    nameInput.style.border = 'none'
+})
+
+const addToCart = function(){
+    if(!cartItems){
+        cartItems +=1;
+        cart.setAttribute('class', 'cart-display')
+        cart.innerText = 'Your Cart: 1 item'
+        main.appendChild(cart);
+    }
+    else{
+        cartItems += 1;
+        cart.innerText = `Your Cart: ${cartItems} items`
+    }
+    cart.style.color = 'blue';
+    cart.style.fontSize = '18px';
+    setTimeout(()=>{
+        cart.style.color = 'black';
+        cart.style.fontSize = '16px';
+    }, 500)
 }
